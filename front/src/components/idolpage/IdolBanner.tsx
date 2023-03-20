8e9ee344bdcafe3ca4363bc94376113fce36d8e1
Lee-hanbin <hanbin0501@gmail.com>
2023-03-20 18:03:33 +0900

    :sparkles: [FE] #3 아이돌 페이지 구조화 중

---
+++ front/src/components/idolpage/IdolBanner.tsx
@@ -0,0 +1,11 @@
+
+
+function IdolBanner () {
+  return (
+    <div>
+      <h1>배너 이미지</h1>
+    </div>
+  )
+};
+
+export default IdolBanner;
--- front/src/routes/IdolPage.tsx
+++ front/src/routes/IdolPage.tsx
@@ -1,19 +1,26 @@
 import { Link, useParams } from "react-router-dom";
 
-import React from "react";
+import IdolBanner from "../components/idolpage/IdolBanner";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 function IdolPage() {
   const params = useParams();
   const idolName = params.idolName;
-
+  
   return (
-    <div>
+    <Wrapper>
+      <IdolBanner />
       <h1>아이돌 디테일 페이지입니다.</h1>
       <h2>이 페이지의 아이돌 이름은 "{`${idolName}`}"입니다. </h2>
       <h2>
         <Link to={`/news/${idolName}`}>이 아이돌의 뉴스 디테일 페이지로 이동</Link>
       </h2>
-    </div>
+    </Wrapper>
   );
 }
 
