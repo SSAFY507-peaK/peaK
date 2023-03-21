58a78b82c0337328b119f75d186b0343f07cc0fd
gyur1kim <qri1114@gmail.com>
2023-03-21 13:34:20 +0900

    :sparkles: [FE] #2 각 페이지 컴포넌트 생성

---
+++ front/src/routes/FullPage/SectionOne.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FullpageWrapper from "./FullpageWrapper";
+
+function SectionOne() {
+  return (
+    <FullpageWrapper backgroundColor="var(--white800-color)">
+      <div>안녕하세요</div>
+    </FullpageWrapper>
+  );
+}
+
+export default SectionOne;
---
+++ front/src/routes/FullPage/SectionThree.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FullpageWrapper from "./FullpageWrapper";
+
+function SectionThree() {
+  return (
+    <FullpageWrapper backgroundColor="#1232df">
+      <div>세 번째  페이지 </div>
+    </FullpageWrapper>
+  );
+}
+
+export default SectionThree;
