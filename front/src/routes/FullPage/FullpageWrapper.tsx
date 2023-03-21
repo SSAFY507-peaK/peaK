3413af76206bc790f82e3bc43bba929861a5127f
gyur1kim <qri1114@gmail.com>
2023-03-21 13:50:23 +0900

    :sparkles: [FE] #2 react-full-page 라이브러리 적용
    - typescript로는 해당 라이브러리를 사용할 수 없음(type을 지정이 안되어있기 때문)
    - d.ts 파일을 만들어도, 새롭게 npm install을 하면 파일이 삭제됨
    - 따라서 js로 컴포넌트 생성함

--- front/src/routes/FullPage/FullpageWrapper.tsx
+++ front/src/routes/FullPage/FullpageWrapper.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 const FullpageWrapper = styled.div<{ backgroundColor: string}>`
-  //height: 100%;
-  height: 100vh;
+  height: 100%;
+  //height: 100vh;
   width: 100%;
   background-color: ${props => props.backgroundColor};
 `
