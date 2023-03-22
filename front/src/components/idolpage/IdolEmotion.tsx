93d042f54ba556ce3d39f4166470c129ee987a67
Lee-hanbin <hanbin0501@gmail.com>
2023-03-22 14:09:55 +0900

    :memo: [FE] #5 아이돌 랭킹 차트 구체화
    - FrameComponent
      - Css 수정
    - IdolEmotion Component
      - EmotionChart와 RankChart 분리
    - IdolEmotionRankChart
      - 차트 생성

--- front/src/components/idolpage/IdolEmotion.tsx
+++ front/src/components/idolpage/IdolEmotion.tsx
@@ -2,6 +2,7 @@ import Emotion from "./sampleImg/Emotion.png"
 import FrameCommponent from './FrameComponent';
 import HelpIcon from '@mui/icons-material/Help';
 import IdolEmotionChart from "./IdolEmotionChart";
+import IdolEmotionRankChart from "./IdolEmotionRankChart";
 import TabComponent from "./TabComponent";
 import styled from "styled-components";
 import { useState } from "react";
@@ -55,14 +56,13 @@ function IdolEmotion() {
             <TabComponent isTab = {!check} title = "랭킹 차트" size ="1.5rem" onClick={() => check? setCheck(false) :setCheck(true)}></TabComponent>
             <HelpIcon sx={{ color: "lightgray" }} />
           </ChartTagFrame>
-          <IdolEmotionChart></IdolEmotionChart>
-          {/* {
-            check 
-            ?
-            <IdolEmotionChart />
-            :
-            <IdolEmotionChart />
-          } */}
+            {
+              check 
+              ?
+              <IdolEmotionChart />
+              :
+              <IdolEmotionRankChart />
+            }
         </>
         }
     ></FrameCommponent>
