6579579830883cd154494bcd6c7b07a8a79dd673
gyur1kim <qri1114@gmail.com>
2023-03-23 16:04:31 +0900

    :lipstick: [FE] #1 메뉴바 아이콘 추가작업
    - mui에서 해당하는 아이콘 import 후 적용
    - 동일한 스타일이 적용되기 때문에 style 객체를 만들었음

--- front/src/components/MenuBar.tsx
+++ front/src/components/MenuBar.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import styled from "styled-components";
 
+import HomeIcon from '@mui/icons-material/Home';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import PersonIcon from '@mui/icons-material/Person';
+import HelpIcon from '@mui/icons-material/Help';
+import LogoutIcon from '@mui/icons-material/Logout';
+
 import { ReactComponent as Logo } from "../assets/peaK.svg";
 import Search from "./Search";
 
@@ -39,7 +47,6 @@ const MenuTab = styled.div`
 const Menu = styled.div`
   width: 100%;
   color: var(--gray400-color);
-
   
   > * {
     display: flex;
@@ -55,6 +62,8 @@ const Menu = styled.div`
   }
 `;
 
+const style = {marginRight: "7px", fontSize: "medium"}
+
 function MenuBar() {
   return (
     <MenuBackground>
@@ -62,17 +71,17 @@ function MenuBar() {
         <Logo />
         <Search width="220px" />
         <MenuTab>
-          <Menu><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>홈</NavLink></Menu>
-          <Menu><NavLink to="/newjeans" className={({ isActive }) => (isActive ? "active" : "")}>관심 아이돌</NavLink></Menu>
-          <Menu><NavLink to="/ranking" className={({ isActive }) => (isActive ? "active" : "")}>랭킹/차트</NavLink></Menu>
-          <Menu><NavLink to="/news" className={({ isActive }) => (isActive ? "active" : "")}>트렌딩</NavLink></Menu>
-          <Menu><NavLink to="/mypage/hanbin" className={({ isActive }) => (isActive ? "active" : "")}>마이페이지</NavLink></Menu>
+          <Menu><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}><HomeIcon style={style}/>홈</NavLink></Menu>
+          <Menu><NavLink to="/newjeans" className={({ isActive }) => (isActive ? "active" : "")}><FavoriteIcon style={style}/>관심 아이돌</NavLink></Menu>
+          <Menu><NavLink to="/ranking" className={({ isActive }) => (isActive ? "active" : "")}><BarChartIcon style={style}/>랭킹/차트</NavLink></Menu>
+          <Menu><NavLink to="/news" className={({ isActive }) => (isActive ? "active" : "")}><TrendingUpIcon style={style}/>트렌딩</NavLink></Menu>
+          <Menu><NavLink to="/mypage/hanbin" className={({ isActive }) => (isActive ? "active" : "")}><PersonIcon style={style}/>마이페이지</NavLink></Menu>
         </MenuTab>
       </MenuSection>
       <MenuSection>
         <MenuTab>
-          <Menu><NavLink to="data">데이터 산출 근거</NavLink></Menu>
-          <Menu><Link to="/">로그아웃</Link></Menu>
+          <Menu><NavLink to="data"><HelpIcon style={style}/>데이터 산출 근거</NavLink></Menu>
+          <Menu><Link to="/"><LogoutIcon style={style}/>로그아웃</Link></Menu>
         </MenuTab>
       </MenuSection>
     </MenuBackground>
