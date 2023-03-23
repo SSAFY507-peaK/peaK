a8c38f8ea900c794c6f94adc7de91c0132ce8f74
shpark0913 <shpark097754@gmail.com>
2023-03-23 16:58:09 +0900

    :lipstick: [FE] #7 메인페이지 UI 변경

---
+++ front/src/components/mainpage/TrendNews.tsx
@@ -0,0 +1,39 @@
+import Carousel from "../Carousel";
+import styled from "styled-components";
+
+const ComponentDiv = styled.div`
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 30px;
+  display: flex;
+  flex-direction: column;
+  padding: 40px;
+  flex: 0.5;
+  padding: 10px 30px;
+  margin-top: 7.5px;
+  margin-left: 5px;
+  margin-right: 7.5px;
+`;
+
+const TitleDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const PurpleH2 = styled.h2`
+  color: var(--purple400-color);
+  margin-left: 5px;
+`;
+
+function TrendNews() {
+  return (
+    <ComponentDiv>
+      <TitleDiv>
+        <h2>트렌딩</h2>
+        <PurpleH2> 뉴스</PurpleH2>
+      </TitleDiv>
+      <Carousel />
+    </ComponentDiv>
+  );
+}
+
+export default TrendNews;
