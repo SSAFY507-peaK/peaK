5992fd7de13b95d7c269d4cec28ff795c85ca678
Lee-hanbin <hanbin0501@gmail.com>
2023-03-27 02:16:49 +0900

    :memo: [FE] #5 응원 한마디 구현
    - IdolChat Component 구체화

--- front/src/components/idolpage/IdolProfile/IdolChat.tsx
+++ front/src/components/idolpage/IdolProfile/IdolChat.tsx
@@ -1,65 +1,91 @@
 import styled from "styled-components";
+import Button from "../../Button";
 import TitleComponent from "../TitleComponent";
 
-// const Wrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   flex: 0.5;
-//   padding: 15px;
-//   width: 100%;
-// `;
 
-const Frame = styled.div`
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 10px;
   box-shadow: 5px;
-  background-color: red;
+  padding: 30px;
+  height: 100%;
 `;
 
 const ChatFrame = styled.div`
+  width: 100%;
+  height: 300px;
+  margin: 25px 13px 40px 25px;
+  padding-right: 25px;
+  overflow-y: auto;
 
+  // 스크롤바 관련 설정
+  &::-webkit-scrollbar {
+    width: 7px;
+    border-radius: 4px;
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background: var(--gray700-color);
+  }
+`;
+const ChatTitle = styled.div`
+  font-size: 1.2rem;
+  font-weight: 700;
+  padding-bottom: 3px;
+`;
+
+const ChatData = styled.div`
+  opacity: 0.8;
+  margin-bottom: 10px;
 `;
 
 const ChatInputFrame = styled.div`
   display: flex;
   flex-direction: row;
-  border-radius: 0px 0px 10px;
-  background-color: yellow;
+  padding-left: 10px;
 `;
 
-
-const Title = styled.div`
-  background-color: var();
-`;
-
-const Button = styled.div`
-  width: 100px;
-  border-radius: 0px 0px 10px;
-  background-color: var(--purple500-color);
-  &:hover {
-    background-color: var(--purple300-color);
-  }
-
-`;
 const ChatInput = styled.input`
   width: 100%;
-  background-color: black;
+  height: 33px;
+  background-color: white;
+  border-radius: 10px;
 `;
 
 
 function IdolChat() {
+  const chat = []
+
+  const tmp = {
+    name: "사랑아럿뜰해",
+    data: "와 고잉은 따라올 수가 없다. 진짜 최고인듯"
+  }
+  for (let i=0 ; i < 30; i++) {
+    chat.push(tmp)
+  }
+
   return (
-    <Frame>
+    <Wrapper>
       <TitleComponent purpletxt="응원" blacktxt="한 마디"/>
-      <Title>응원 한 마디</Title>
-      <ChatFrame>여기는 댓글들이 달리는 곳</ChatFrame>
+      <ChatFrame>
+        {
+          chat.map((e, idx) => {
+            return(
+              <>
+                <ChatTitle>{e.name}</ChatTitle>
+                <ChatData>{e.data}</ChatData>
+              </>
+            )
+          })
+        }
+      </ChatFrame>
       <ChatInputFrame>      
         <ChatInput placeholder="여기는 댓글을 적는 곳"></ChatInput>
-        <Button>응원 남기기</Button>
+        <Button buttonColor="purple" width="100px">응원</Button>
       </ChatInputFrame>
-    </Frame>
+    </Wrapper>
   )
 }
 
