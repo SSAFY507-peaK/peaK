4a99937557a19b7e5eff15af0663ce68a468482f
gyur1kim <qri1114@gmail.com>
2023-03-27 14:21:41 +0900

    :lipstick: [FE] #13 로그인 버튼 수정
    - 이미지는 왼쪽에 가고 텍스트는 가운데 오도록 수정
      - 버튼 전체를 감싸는 Wrapper를 생성했음

--- front/src/components/LoginModal/LoginButton.tsx
+++ front/src/components/LoginModal/LoginButton.tsx
@@ -1,77 +1,68 @@
 import styled from "styled-components";
-import kakaoLogin from "../../assets/kakao_round.png"
-import naverLogin from "../../assets/naver_round.png"
+import kakaoLogin from "../../assets/kakao_round.png";
+import naverLogin from "../../assets/naver_round.png";
 
-const LoginButton = styled.button`
+const ButtonWrapper = styled.button`
   width: 250px;
   height: 45px;
-  border-radius: 30px;
-  margin-bottom: 10px;
-  
-  background-color: white;
-  color: var(--gray200-color);
-  transition: all 100ms ease-in;
-  
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  
-  &:hover {
-    color: white;
-  }
-`
-const NaverLoginButton = styled(LoginButton)`
-  border: 2px solid #03C75A;
-  &:hover {
-    background-color: #03C75A;
-  }
-`
-const KakaoLoginButton = styled(LoginButton)`
-  border: 2px solid #FEE500;
-  &:hover {
-    background-color: #FEE500;
-    color: rgba(0, 0, 0, 0.9);
-  }
-`
-
+  margin-bottom: 15px;
+  background-color: transparent;
+  position: relative;
+`;
 const IconSection = styled.img`
   padding: 0;
   margin: 0;
   width: 45px;
   height: 45px;
-  &>img{
-    width: 100%;
-    height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+`;
+
+const LoginButton = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
+
+  background-color: white;
+  color: var(--gray200-color);
+  transition: all 100ms ease-in;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NaverLoginButton = styled(LoginButton)`
+  border: 2px solid #03c75a;
+  &:hover {
+    background-color: #03c75a;
+    color: white;
   }
-`
-const ContextSection = styled.div`
-  flex: 1;
-`
+`;
+const KakaoLoginButton = styled(LoginButton)`
+  border: 2px solid #fee500;
+  &:hover {
+    background-color: #fee500;
+  }
+`;
 
-function NaverLogin ()  {
+function NaverLogin() {
   return (
-    <NaverLoginButton>
-      <IconSection>
-        <img src={naverLogin} alt="네이버 소셜 로그인"/>
-      </IconSection>
-      <ContextSection>
-        네이버로 로그인
-      </ContextSection>
-    </NaverLoginButton>
+    <ButtonWrapper>
+      <IconSection src={naverLogin} alt="네이버 소셜 로그인" />
+      <NaverLoginButton>네이버로 로그인</NaverLoginButton>
+    </ButtonWrapper>
   );
 }
 
-function KakaoLogin ()  {
+function KakaoLogin() {
   return (
-    <KakaoLoginButton>
-      <IconSection>
-        <img src={kakaoLogin} alt="카카오 소셜 로그인"/>
-      </IconSection>
-      <ContextSection>
-        카카오로 로그인
-      </ContextSection>
-    </KakaoLoginButton>
+    <ButtonWrapper>
+      <IconSection src={kakaoLogin} alt="카카오 소셜 로그인"></IconSection>
+      <KakaoLoginButton>카카오로 로그인</KakaoLoginButton>
+    </ButtonWrapper>
   );
 }
 
-export {KakaoLogin, NaverLogin} ;
+export { KakaoLogin, NaverLogin };
