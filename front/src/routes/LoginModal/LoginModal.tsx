0d9941508c63f9435227127fe93c1c353552e6dd
gyur1kim <qri1114@gmail.com>
2023-03-27 17:05:08 +0900

    :sparkles: [FE] #13 로그인 모달창 구현
    - x버튼을 누르면 모달창 닫힘
    - 모달 이외의 영역을 클릭하면 모달창 닫힘

--- front/src/routes/LoginModal/LoginModal.tsx
+++ front/src/routes/LoginModal/LoginModal.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styled from 'styled-components'
-import {useLocation} from "react-router-dom";
+
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { KakaoLogin, NaverLogin } from "../../components/LoginModal/LoginButton";
+import useOnClickOutside from "../../_hooks/useOnClickOutside";
+
+
+type LoginModalProps = {
+  setLoginOpen: (loginOpen: boolean) => void;
+}
 
 const ModalBackground = styled.div`
   background-color: rgba(0, 0, 0, 0.5);
+  //filter: blur(10px);
+  //-webkit-filter: blur(10px);
   width: 100vw;
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  
+  position: absolute;
+  top: 0;
+  left: 0;
   z-index: 999;
 `
+
 const ModalWrapper = styled.div`
   width: 400px;
   height: 450px;
   background-color: white;
-  border-radius: 30px;
+  border-radius: 20px;
   padding: 30px;
   
   display: flex;
@@ -62,13 +73,14 @@ const ButtonWrapper = styled.div`
   }
 `;
 
-function LoginModal() {
-  const location = useLocation();
-  console.log(location);
+function LoginModal({ setLoginOpen }:LoginModalProps) {
+  const ref = useRef();
+  useOnClickOutside(ref, () => {setLoginOpen(false)})
+
   return (
     <ModalBackground>
-      <ModalWrapper>
-        <CloseButton>X</CloseButton>
+      <ModalWrapper ref={ref}>
+        <CloseButton onClick={() => setLoginOpen(false)}>X</CloseButton>
         <LogoWrapper>
           <Logo />
           <h2>로그인하기</h2>
