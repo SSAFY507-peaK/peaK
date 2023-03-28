4fce95eb76963453df909cd3e07772729a35e370
Lee-hanbin <hanbin0501@gmail.com>
2023-03-28 10:17:52 +0900

    :sparkles: [FE] #15 react-ga 를 통한 google analytics 적용을 위해 router 수정

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,74 +1,20 @@
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-
-import ChartPage from "./routes/ChartPage";
-import ErrorPage from "./routes/ErrorPage";
 import { GlobalStyle } from "./components/globalStyle.js";
-import IdolPage from "./routes/IdolPage";
-import Layout from "./routes/Layout";
-import MainPage from "./routes/MainPage";
-import MyPage from "./routes/MyPage";
-import NewsDetailPage from "./routes/NewsDetailPage";
-import NewsPage from "./routes/NewsPage";
-import NicknamePage from "./routes/NicknamePage";
 import { Provider } from "react-redux";
-import RankingLayout from "./routes/RankingLayout";
-import RankingPage from "./routes/RankingPage";
-import SelectPage from "./routes/SelectPage";
-import TmpFullPage from "./routes/FullPage/tmpFullPage";
+import ReactGA from 'react-ga'
+import { RouterProvider } from "react-router";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import router from "./Router";
 import { store } from "./_store/store";
 
-import ReactGA from 'react-ga'
-import RouteChangeTracker from "./_utils/RouteChangeTracker";
-
-
-
-
-
-
 // import FullPage from "./routes/FullPage/FullPage"
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      {/*<Route path="/intro" element={ <FullPage /> } />*/}
-      <Route path="/intro" element={ <TmpFullPage /> } />
-      <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
-        <Route errorElement={<ErrorPage />}>
-          <Route index element={<MainPage />} />
-          <Route path="signup/nickname" element={<NicknamePage />} />
-          <Route path="signup/select" element={<SelectPage />} />
-
-          <Route path="ranking" element={<RankingLayout />}>
-            <Route index element={<RankingPage />} />
-            <Route path="chart" element={<ChartPage />} />
-          </Route>
-
-          <Route path="news" element={<NewsPage />} />
-          <Route path="news/:idolName" element={<NewsDetailPage />} />
-
-          <Route path=":idolName" element={<IdolPage />} />
-          <Route path="mypage/:userName" element={<MyPage />} />
-        </Route>
-      </Route>
-    </Route>,
-  ),
-);
-
-const container = document.getElementById("root")!;
-const root = createRoot(container);
 
 if (process.env.REACT_APP_GOOGLE_ANALYTICS_TRANKING_ID) {
   ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRANKING_ID);
 }
-// RouteChangeTracker();
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
