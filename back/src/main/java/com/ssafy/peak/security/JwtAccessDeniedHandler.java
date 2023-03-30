4102c919df5cdf26cd6961fc1926b992e78ea074
박주희 <dam2.dev@gmail.com>
2023-03-30 15:02:32 +0900

    :bug: [BE] #16 Component annotation 추가

--- back/src/main/java/com/ssafy/peak/security/JwtAccessDeniedHandler.java
+++ back/src/main/java/com/ssafy/peak/security/JwtAccessDeniedHandler.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
