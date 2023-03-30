0113ee25282fba37c8af89da897563a5c7471ba1
shpark0913 <shpark097754@gmail.com>
2023-03-30 17:34:43 +0900

    :sparkles: [FE] #10 랭킹 페이지 구현 시작

--- front/src/routes/RankingPage.tsx
+++ front/src/routes/RankingPage.tsx
@@ -1,5 +1,39 @@
+import styled from "styled-components";
+
+const AllRankDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  height: 100%;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+    border-radius: 4px;
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background: var(--gray700-color);
+  }
+`;
+
+const IdolRankDiv = styled.div`
+  display: flex;
+  width: 100%;
+  height: 13vh;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+const RankDiv = (props: any) => {
+  return <IdolRankDiv>{props.rank}</IdolRankDiv>;
+};
+
 function RankingPage() {
-  return <div></div>;
+  const items = [{ rank: 1, name: "BTS" }];
+  return <AllRankDiv>{items.map((item, idx) => RankDiv(item))}</AllRankDiv>;
 }
 
 export default RankingPage;
