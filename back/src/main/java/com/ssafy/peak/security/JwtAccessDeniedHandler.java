00b30396535af290b338562a5447b5f58b2b5ca6
박주희 <dam2.dev@gmail.com>
2023-03-30 09:45:56 +0900

    :sparkles: [BE] #16 JwtAccessDeniedHandler 생성

---
+++ back/src/main/java/com/ssafy/peak/security/JwtAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package com.ssafy.peak.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+		AccessDeniedException accessDeniedException) throws IOException, ServletException {
+		response.sendError(HttpServletResponse.SC_FORBIDDEN, "해당 기능을 요청할 권한이 없습니다.");
+	}
+}
