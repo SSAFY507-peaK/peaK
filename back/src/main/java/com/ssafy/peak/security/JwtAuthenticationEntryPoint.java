5aa9d12161ca8058d838fc0b932fabe2c7d6c499
박주희 <dam2.dev@gmail.com>
2023-03-30 09:51:13 +0900

    :sparkles: [BE] #16 JwtAuthenticationEntryPoint 생성

---
+++ back/src/main/java/com/ssafy/peak/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package com.ssafy.peak.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException, ServletException {
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "액세스 토큰 오류입니다.");
+	}
+}
