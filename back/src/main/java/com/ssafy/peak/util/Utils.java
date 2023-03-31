c894e5fe6e6bf6b84162947cbe8c298861333979
박주희 <dam2.dev@gmail.com>
2023-03-31 17:34:42 +0900

    :recycle: [BE] Utils class로 상수 사용

---
+++ back/src/main/java/com/ssafy/peak/util/Utils.java
@@ -0,0 +1,13 @@
+package com.ssafy.peak.util;
+
+public class Utils {
+
+	// JWT
+	public static final String ROLE = "role";
+	public static final String AUTHORIZATION = "Authorization";
+	public static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+	// KAKAO OAUTH
+	public static final String KAKAO_ACCOUNT = "kakao_account";
+	public static final String EMAIL = "email";
+}
