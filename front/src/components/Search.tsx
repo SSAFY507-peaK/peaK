d9aba93943a5eb4ddbb21f677b98bae37f53da96
shpark0913 <shpark097754@gmail.com>
2023-04-03 14:20:09 +0900

    :sparkles: [FE] #27 검색 기능 구현 완료

--- front/src/components/Search.tsx
+++ front/src/components/Search.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
 import SearchIcon from "@mui/icons-material/Search";
+import UseOnClickOutside from "../_hooks/useOnClickOutside";
 import styled from "styled-components";
 
 type WrapperType = {
@@ -37,6 +38,7 @@ const SearchInput = styled.input`
 const SearchAllResult = styled.div`
   width: 95%;
   background-color: white;
+  box-shadow: 0px 0px 10px -2px #cfcdcd;
   padding: 5px 10px 10px 10px;
   position: absolute;
   top: 33px;
@@ -46,17 +48,35 @@ const SearchAllResult = styled.div`
 const SearchResultDiv = styled.div`
   cursor: pointer;
   font-size: 13px;
-  padding: 7px 0px;
+  padding: 10px 8px;
+  &:hover {
+    font-weight: bold;
+  }
   border-bottom: 0.5px solid #dddddd;
+  :last-child {
+    border-bottom: none;
+    padding: 10px 8px 5px 8px;
+  }
 `;
 
 function Search({ width }: WrapperType) {
   const [search, setSearch] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
+  const searchRef = useRef<any>();
 
   const onChange = (e: any) => {
     console.log(search);
     setSearch(e.target.value);
+    setIsClicked(true);
   };
+
+  const onClick = (e: any) => {
+    setIsClicked(true);
+  };
+
+  UseOnClickOutside(searchRef, () => {
+    setIsClicked(false);
+  });
 
   const idols = [
     { name: "세븐틴" },
@@ -77,18 +97,24 @@ function Search({ width }: WrapperType) {
   });
 
   return (
-    <SearchDiv>
+    <SearchDiv ref={searchRef}>
       <Wrapper width={width}>
-        <SearchInput placeholder="아이돌 이름을 입력해주세요" value={search} onChange={onChange} />
+        <SearchInput
+          placeholder="아이돌 이름을 입력해주세요"
+          value={search}
+          onChange={onChange}
+          onClick={onClick}
+        />
         <SearchIcon sx={{ color: "var(--gray600-color)" }} />
       </Wrapper>
-      {search !== "" && (
+      {search !== "" && isClicked && (
         <SearchAllResult>
           {filterIdol.map(idol => (
             <SearchResultDiv
               onClick={e => {
                 e.preventDefault();
                 alert(idol.name);
+                setSearch("");
               }}
             >
               {idol.name}
