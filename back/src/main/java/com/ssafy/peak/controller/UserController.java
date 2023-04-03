abbb22fbe7365961904efee93314ca61997592b8
박주희 <dam2.dev@gmail.com>
2023-04-03 21:53:16 +0900

    :art: [BE] #19 UserController 주석 추가, Response 변수명 변경

--- back/src/main/java/com/ssafy/peak/controller/UserController.java
+++ back/src/main/java/com/ssafy/peak/controller/UserController.java
@@ -44,6 +44,7 @@ public class UserController {
 
 	/**
 	 * 회원 가입
+	 * (OAuth2 login 성공 후)
 	 */
 	@GetMapping("/sign-up")
 	public ResponseEntity signup(
@@ -91,11 +92,13 @@ public class UserController {
 	@Transactional
 	public ResponseEntity reissue(@RequestHeader("Authorization") String token) {
 
-		token = token.split(" ")[1];
+		// header에 token 꺼내서 Bearer 떼고 재발급 하러 가기
+		token = token.split(Utils.BLANK)[1];
 		JwtTokenDto tokenDto = jwtTokenProvider.reissue(token);
 
+		// header에도 토큰 넣어주기
 		HttpHeaders httpHeaders = new HttpHeaders();
-		httpHeaders.add(Utils.TOKEN, Utils.BEARER_TOKEN_PREFIX + tokenDto.getToken());
+		httpHeaders.add(Utils.AUTHORIZATION, Utils.BEARER_TOKEN_PREFIX + tokenDto.getToken());
 
 		return ResponseEntity.ok().headers(httpHeaders).body(tokenDto);
 	}
