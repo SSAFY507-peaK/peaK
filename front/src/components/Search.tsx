4adbb3e7739e93702e22ff8d98582fef45589909
shpark0913 <shpark097754@gmail.com>
2023-04-03 13:17:18 +0900

    :sparkles: [FE] #27 검색 기능 구현
    - idols에 등록된 아이돌 검색 기능 구현
    - 클릭 유무에 따라 렌더링 여부 구현 추가로 해야 함

--- front/src/components/Search.tsx
+++ front/src/components/Search.tsx
@@ -1,12 +1,19 @@
+import { useEffect, useRef, useState } from "react";
+
 import SearchIcon from "@mui/icons-material/Search";
 import styled from "styled-components";
 
 type WrapperType = {
   width?: string;
-}
+};
+
+const SearchDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
 
 const Wrapper = styled.div<WrapperType>`
-  position: relative;
   display: flex;
   align-items: center;
   border: 1px solid var(--gray700-color);
@@ -27,12 +34,69 @@ const SearchInput = styled.input`
   }
 `;
 
-function Search({width}: WrapperType) {
+const SearchAllResult = styled.div`
+  width: 95%;
+  background-color: white;
+  padding: 5px 10px 10px 10px;
+  position: absolute;
+  top: 33px;
+  left: 5px;
+`;
+
+const SearchResultDiv = styled.div`
+  cursor: pointer;
+  font-size: 13px;
+  padding: 7px 0px;
+  border-bottom: 0.5px solid #dddddd;
+`;
+
+function Search({ width }: WrapperType) {
+  const [search, setSearch] = useState("");
+
+  const onChange = (e: any) => {
+    console.log(search);
+    setSearch(e.target.value);
+  };
+
+  const idols = [
+    { name: "세븐틴" },
+    { name: "세꼬시" },
+    { name: "재산세" },
+    { name: "설날세배" },
+    { name: "Exo" },
+    { name: "트와이스" },
+    { name: "아이브" },
+    { name: "있지" },
+    { name: "(아이들)" },
+    { name: "뉴진스" },
+    { name: "BTS" },
+  ];
+
+  const filterIdol = idols.filter(idol => {
+    return idol.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+  });
+
   return (
-    <Wrapper width={width}>
-      <SearchInput placeholder="아이돌 이름을 입력해주세요" />
-      <SearchIcon sx={{ color: "var(--gray600-color)" }} />
-    </Wrapper>
+    <SearchDiv>
+      <Wrapper width={width}>
+        <SearchInput placeholder="아이돌 이름을 입력해주세요" value={search} onChange={onChange} />
+        <SearchIcon sx={{ color: "var(--gray600-color)" }} />
+      </Wrapper>
+      {search !== "" && (
+        <SearchAllResult>
+          {filterIdol.map(idol => (
+            <SearchResultDiv
+              onClick={e => {
+                e.preventDefault();
+                alert(idol.name);
+              }}
+            >
+              {idol.name}
+            </SearchResultDiv>
+          ))}
+        </SearchAllResult>
+      )}
+    </SearchDiv>
   );
 }
 
