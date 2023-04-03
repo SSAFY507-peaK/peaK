7d085ffed9ebd1e778b97aafbbef0d6d7b877db0
gyur1kim <qri1114@gmail.com>
2023-04-03 13:20:04 +0900

    :sparkles: [FE] #20  차트선택 페이지
    - 아직 type에 오류 존재..

--- front/src/components/rankingpage/RankChart.tsx
+++ front/src/components/rankingpage/RankChart.tsx
@@ -13,10 +13,19 @@ import { useState } from "react";
 //   let D7 = new Date(now.setDate(now.getDate() - 7))
 // }
 
-function RankChart() {
+type ChartIdolObjectType = {
+  name?: string;
+  data?: number[];
+  type: string;
+  lineStyle: {width: number};
+}
+
+function RankChart(props:any) {
+  const idolLegend: string[] = props.idolLegend
+  const chartIdol: ChartIdolObjectType[] = props.chartIdol
   let labels = ["D-6", "D-5", "D-4", "D-3", "D-2", "D-1", "오늘"];
   const [options, setOptions] = useState({
-    color: ["#93F2A2", "#DAB4B4", "#FFD99E", "#FCA7A7", "#9FC6F4", "#A869CD"],
+    color: ["#4CD7F6", "#6DBFFF", "#7166F9", "#C74BF6", "#F946FF"],
     tooltip: {
       trigger: "axis",
       axisPointer: {
@@ -24,7 +33,7 @@ function RankChart() {
       },
     },
     legend: {
-      data: ["세븐틴", "에잇틴", "나인틴", "투엔티", "2NE1"],
+      data: idolLegend,
       orient: "vertical",
       top: "170vw",
       right: "right",
@@ -47,48 +56,49 @@ function RankChart() {
         inverse: true,
       },
     ],
-    series: [
-      {
-        name: "세븐틴",
-        type: "line",
-        data: [62, 11, 11, 11, 10, 3, 1],
-        lineStyle: {
-          width: 3,
-        },
-      },
-      {
-        name: "에잇틴",
-        type: "line",
-        data: [2, 3, 4, 5, 6, 7, 9],
-        lineStyle: {
-          width: 3,
-        },
-      },
-      {
-        name: "나인틴",
-        type: "line",
-        data: [15, 15, 13, 13, 15, 15, 18],
-        lineStyle: {
-          width: 3,
-        },
-      },
-      {
-        name: "투엔티",
-        type: "line",
-        data: [7, 8, 9, 8, 9, 8, 19],
-        lineStyle: {
-          width: 3,
-        },
-      },
-      {
-        name: "2NE1",
-        type: "line",
-        data: [1, 1, 1, 1, 1, 1, 2],
-        lineStyle: {
-          width: 3,
-        },
-      },
-    ],
+    series: chartIdol
+    // [
+    //   {
+    //     name: "세븐틴",
+    //     type: "line",
+    //     data: [62, 11, 11, 11, 10, 3, 1],
+    //     lineStyle: {
+    //       width: 3,
+    //     },
+    //   },
+    //   {
+    //     name: "에잇틴",
+    //     type: "line",
+    //     data: [2, 3, 4, 5, 6, 7, 9],
+    //     lineStyle: {
+    //       width: 3,
+    //     },
+    //   },
+    //   {
+    //     name: "나인틴",
+    //     type: "line",
+    //     data: [15, 15, 13, 13, 15, 15, 18],
+    //     lineStyle: {
+    //       width: 3,
+    //     },
+    //   },
+    //   {
+    //     name: "투엔티",
+    //     type: "line",
+    //     data: [7, 8, 9, 8, 9, 8, 19],
+    //     lineStyle: {
+    //       width: 3,
+    //     },
+    //   },
+    //   {
+    //     name: "2NE1",
+    //     type: "line",
+    //     data: [1, 1, 1, 1, 1, 1, 2],
+    //     lineStyle: {
+    //       width: 3,
+    //     },
+    //   },
+    // ],
   });
 
   return (
