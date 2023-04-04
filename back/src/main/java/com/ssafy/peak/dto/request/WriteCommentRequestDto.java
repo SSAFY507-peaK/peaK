ddef5d73c1f9a6582bfa621cf049aca56fc64687
박주희 <dam2.dev@gmail.com>
2023-04-05 02:34:51 +0900

    :sparkles: [BE] #19 아이돌에게 응원 한 마디 api 구현

---
+++ back/src/main/java/com/ssafy/peak/dto/request/WriteCommentRequestDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.peak.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class WriteCommentRequestDto {
+	private String content;
+}
