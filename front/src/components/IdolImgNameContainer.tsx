57fcab1d906a27763592bf4fd488944cee831dfb
gyur1kim <qri1114@gmail.com>
2023-04-06 13:04:18 +0900

    :recycle: [FE] #7 메인페이지 랭킹 수정
    - grid로 수정함

--- front/src/components/IdolImgNameContainer.tsx
+++ front/src/components/IdolImgNameContainer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from "styled-components";
 import { IdolImage } from "./SignUpPage/IdolComponents";
 
-const IdolImageNameContainer = styled.div`
-  width: 100%;
+const IdolImageNameContainer = styled.div<{ width?: string }>`
+  width: ${props => props.width? props.width : "100%"};
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -16,15 +16,15 @@ const IdolName = styled.div`
 
 type IdolImgNameContainerProps = {
   idol: string,
-  width?: string,
   url: string,
-  className: string,
-  onClick: (idol: string) => void,
+  width?: string,
+  className?: string,
+  onClick?: (idol: string) => void,
 }
 function IdolImgNameContainer({idol, width, url, className, onClick }: IdolImgNameContainerProps) {
 
   return (
-    <IdolImageNameContainer onClick={() => onClick(idol) }>
+    <IdolImageNameContainer onClick={() => onClick && onClick(idol) }>
       <IdolImage width={width} url={url} className={className} />
       <IdolName>{ idol }</IdolName>
     </IdolImageNameContainer>
@@ -32,3 +32,4 @@ function IdolImgNameContainer({idol, width, url, className, onClick }: IdolImgNa
 }
 
 export default React.memo(IdolImgNameContainer);
+export { IdolImageNameContainer, IdolName }
