073a00f7cfd3d228787ffd0a5c01dc9ed5748ff6
gyur1kim <qri1114@gmail.com>
2023-04-07 04:44:35 +0900

    :bug: [FE] #38 비동기 작업을 통해 좋아하는 아이돌 받아오기
    - async/await를 통해 토큰 값이 유효한지 확인한 뒤 좋아하는 아이돌 요청

--- front/src/routes/Layout.tsx
+++ front/src/routes/Layout.tsx
@@ -41,19 +41,32 @@ function Layout() {
   let userId = useAppSelector(state => state.userInfo.userId);
   let token = useAppSelector(state => state.userInfo.TOKEN);
   let nickname = useAppSelector(state => state.userInfo.nickname);
-  // let favIdols = useAppSelector(state => state.userInfo.favIdols);
+  let favIdols = useAppSelector(state => state.userInfo.favIdols);
 
-  /** 토큰에 대한 useEffect */
-  useEffect(() => {
+  const userInfoFunction = async () => {
     if (query.get('token')) {   // 로그인 후에는 query에 token이 들어있으므로 그 값을 저장하자.
       dispatch(CreateTOKEN(query.get('token')));
       dispatch(CreateUserId(query.get('userId')));
       dispatch(CreateNickname(query.get('nickname')));
+
+      await axios.get(`${BASE_URL}/api/interest/list`, {
+        headers: {
+          Authorization: token
+        }
+      })
+        .then(response => {
+          console.log(response.data);
+          return response.data.idols
+        })
+        .then(idols => dispatch(CreateFavIdols(idols)))
+        .catch(error => console.log(error));
+
+      ga('set', userId, userId);
       window.history.pushState({}, "", "/")
     }
     else {
       if (token) {   // 토큰이 있으면 토큰 유효성을 검사하자
-        axios.post(`${BASE_URL}/api/user/reissue`, {}, {
+        await axios.post(`${BASE_URL}/api/user/reissue`, {}, {
           headers: {
             Authorization: token
           }
@@ -70,31 +83,32 @@ function Layout() {
             window.localStorage.clear();
             navigate('/intro');
           })
+
+        // 리덕스에 값이 없으면 요청하기
+        await favIdols.length === 0 && axios.get(`${BASE_URL}/api/interest/list`, {
+          headers: {
+            Authorization: token
+          }
+        })
+          .then(response => {
+            console.log(response.data);
+            return response.data.idols
+          })
+          .then(idols => dispatch(CreateFavIdols(idols)))
+          .catch(error => console.log(error));
       }
       else {    // 토큰이 없으면 intro로 보내버리자.
         navigate('/intro');
       }
     }
-  }, [navigate]);
+  }
+
+  userInfoFunction();
 
   /** ga에 대한 것, userId가 존재하면 ga 시작 */
   useEffect(() => {
     userId.length && ga('set', userId, userId);
   }, [userId]);
-
-  useEffect(() => {
-    axios.get(`${BASE_URL}/api/interest/list`, {
-      headers: {
-        Authorization: token
-      }
-    })
-      .then(response => {
-        console.log(response.data);
-        return response.data.idols
-      })
-      .then(idols => dispatch(CreateFavIdols(idols)))
-      .catch(error => console.log(error));
-  }, [])
 
   RouteChangeTracker();
 
